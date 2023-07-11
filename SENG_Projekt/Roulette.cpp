f69b736179c2d806ad3b5a9ddb2b1faa5206645e
fabia <fabia@PC_Fabi>
2023-07-11 12:30:39 +0200

    Erste Schritte (Includes, int main)

---
+++ SENG_Projekt/Roulette.cpp
@@ -0,0 +1,20 @@
+/*
+ * Roulette.cpp
+ *
+ *  Created on: 11.07.2023
+ *      Author: fabia
+ */
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <string.h>
+
+int main() {
+
+
+	return 0;
+}
+
+
